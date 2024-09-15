daf9e05d92236b23f3ff94588c1691932e8a8123
virtual-armor <virtualinfoarmor@gmail.com>
2024-09-15 17:30:52 -0500

    Added command line functionality for IP range.
    User can now specify. IP range.

--- net_scan.py
+++ net_scan.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
 import scapy.all as sc
+import subprocess as sp
+import optparse as op
+import re
 
+#get ip address and host range
+def get_ip():
+	parser = op.OptionParser()
+	parser.add_option("-p", "--ip_address", dest="ip_address", help="Enter the full IP address range (including CIDR range). E.g. 170.2.44.0/24")
+	(options, values) = parser.parse_args()
+	if not options.ip_address:
+		parser.error("[-] An IP address range was not specified")
+	else:
+		 return options
 #discover clients on the same network
 def scan(ip):
 	#ARP - Address Resolution Protocol 
@@ -33,10 +45,19 @@ def scan(ip):
 	#Set srp verbosity to false to not include extraneous information.
 	ans = sc.srp(arp_ether_packet, timeout=1.1, verbose=False)[0]
 	#Print header
-	print("IP\t\t\tMAC Address\n-------------------------------------------")
+	device_list = []
 	#print IP and MAC of connected device that replied
 	for a in ans:
-		print(a[1].psrc + "\t\t" + a[1].hwsrc)
-	#ans.show()	
-	
-scan("172.16.251.0/24")
+		device_dict = {"ip":a[1].psrc, "mac":a[1].hwsrc}
+		device_list.append(device_dict)
+	return device_list
+
+def print_connected_devices(devices):
+	print("IP\t\t\tMAC Address\n---------------------------------------------")
+	for d in devices:
+		print(d["ip"] + "\t\t" + d["mac"])
+
+ip_result = get_ip()
+result = scan(ip_result.ip_address)
+#result = scan("172.16.251.0/24")
+print_connected_devices(result)
